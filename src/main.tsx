437a1c2440db9d2c31212f7791049c45af8f4cd6
wt.jeong <wt.jeong@mac-mini.local>
2026-04-27 03:39:21 +0900

    feat: add production metadata and runtime title sync

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { appConfig } from './config/appConfig';
 import { setupGlobalErrorHandlers } from './lib/errors';
 import './index.css';
 
 setupGlobalErrorHandlers();
+document.title = appConfig.appTitle;
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
